ecc8fbd657acfc2dcdab0a56021fccc80d93e23d
llitsbeyourbet <suputtida.pu@rmuti.ac.th>
2026-06-25 14:04:38 +0700

    fix code

--- frontend/pages/Register.jsx
+++ frontend/pages/Register.jsx
@@ -31,7 +31,7 @@ const Register = () => {
     setLoading(true);
 
     try {
-      const res = await fetch("${API_URL}/api/auth/register", {
+      const res = await fetch(`${API_URL}/api/auth/register`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, name, email, password }),
