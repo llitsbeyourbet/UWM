a157adc9221ef0ba681a1c01613df08a6e03853c
Waraporn <sxit5032@gmail.com>
2026-06-28 17:48:12 +0700

    แก้ไขขนาดหน้าและแถบล่าง

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -15,7 +15,7 @@ import CheckIn from "./pages/CheckIn";
 import AdminDashboard from "./pages/AdminDashboard";
 import ReviewForm from "./pages/ReviewForm";
 import ForgotPassword from "./pages/ForgotPassword";
-
+import "./assets/AppLayout.css"
 
 
 function App() {
@@ -24,27 +24,31 @@ function App() {
 
   return (
     <>
-      {!hideNavbar && <BottomNavbar />}
+      <div className="page-wrapper">
+        <div className="app-layout">
 
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>} />
-        <Route path="/CreateActivities" element={<CreateActivities />} />
-        <Route path="/activities" element={<ActivityDetail />} />
-        <Route path="/activity-detail" element={<ActivityDetail />} />
-        <Route path="/search" element={<Search />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/notifications" element={<Notifications />} />
-        <Route path="/edit-profile" element={<EditProfile />} />
-        <Route path="/edit-activity/:id" element={<EditActivity />} />
-        <Route path="/checkin/:activityId" element={<CheckIn />} />
-        <Route path="/admin" element={<AdminDashboard />} />
-        <Route path="/review/:activityId" element={<ReviewForm />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
+          {!hideNavbar && <BottomNavbar />}
 
-      </Routes>
-    </> 
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>} />
+            <Route path="/CreateActivities" element={<CreateActivities />} />
+            <Route path="/activities" element={<ActivityDetail />} />
+            <Route path="/activity-detail" element={<ActivityDetail />} />
+            <Route path="/search" element={<Search />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/notifications" element={<Notifications />} />
+            <Route path="/edit-profile" element={<EditProfile />} />
+            <Route path="/edit-activity/:id" element={<EditActivity />} />
+            <Route path="/checkin/:activityId" element={<CheckIn />} />
+            <Route path="/admin" element={<AdminDashboard />} />
+            <Route path="/review/:activityId" element={<ReviewForm />} />
+          </Routes>
+
+        </div>
+      </div>
+    </>
   );
 }
 
