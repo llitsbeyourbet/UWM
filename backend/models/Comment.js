8a24ffb2329ed869660b4fe73d2f2a382d086c98
llitsbeyourbet <suputtida.pu@rmuti.ac.th>
2026-07-12 14:16:04 +0700

    add comment row

--- backend/models/Comment.js
+++ backend/models/Comment.js
@@ -5,6 +5,8 @@ const Comment = sequelize.define("Comment", {
   activityId: { type: DataTypes.INTEGER, allowNull: false },
   userId: { type: DataTypes.INTEGER, allowNull: false },
   comment: { type: DataTypes.TEXT, allowNull: false },
+  isPublic: {type: DataTypes.BOOLEAN, allowNull: false, defaultValue: false,
+},
 });
 
 module.exports = Comment;
