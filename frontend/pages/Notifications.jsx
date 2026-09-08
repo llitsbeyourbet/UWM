4127c28ca41ceaf3df33e6acb6511bcbc0221a9c
llitsbeyourbet <suputtida.pu@rmuti.ac.th>
2026-09-08 19:27:06 +0700

    edit again

--- frontend/pages/Notifications.jsx
+++ frontend/pages/Notifications.jsx
@@ -2,7 +2,7 @@ import API_URL from "../config";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useSocket } from "../src/context/SocketContext";
-import { useAlert } from "../src/context/AlertContext";
+import { useAlert } from "../components/AlertModal";
 import "../styles/Notifications.css";
 
 function Notifications() {
